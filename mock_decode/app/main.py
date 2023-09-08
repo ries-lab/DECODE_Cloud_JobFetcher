12996a129f4c91d3ea095f2be5755dce384a914c
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2023-09-08 09:10:53 +0200

    print args and env

--- mock_decode/app/main.py
+++ mock_decode/app/main.py
@@ -1,6 +1,8 @@
 import sys
+import os
 
 
 if __name__ == "__main__":
     print(sys.argv)
-    print(sys.argv)
+    for name, value in os.environ.items():
+        print("{0}: {1}".format(name, value))

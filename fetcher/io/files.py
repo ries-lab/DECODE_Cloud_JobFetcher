6f3015296d63f752b8e4698bea96f1a28a588ddc
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2023-09-19 18:01:50 +0200

    proto of api filesystem

--- fetcher/io/files.py
+++ fetcher/io/files.py
@@ -3,6 +3,88 @@ from pathlib import Path
 
 import requests
 
+from fetcher.api import worker
+
+
+class PathAPIUp:
+    def __init__(
+        self,
+        path: str | Path,
+        path_api: str | Path,
+        api: worker.JobAPI,
+    ):
+        """
+
+        Args:
+            path:
+            path_api: path to make path relative to in API world
+            api: api backend
+        """
+        self._path = Path(path) if not isinstance(path, Path) else path
+        self._path_api = Path(path_api) if not isinstance(path_api, Path) else path_api
+        self._api = api
+
+    @property
+    def path_api_rel(self):
+        """
+        API-relative path
+        """
+        return self._path.relative_to(self._path_api)
+
+    def __str__(self):
+        return str(self._path)
+
+    def __repr__(self):
+        return (
+            f"PathAPIUp({repr(self._path)}, {repr(self._api)})"
+            f"PathAPIUp({repr(self._path)}, {repr(self._api)})"
+        )
+
+    def __getattr__(self, attr):
+        # Delegate to the underlying _path object
+        return getattr(self._path, attr)
+
+    def push(self):
+        self._api.put_file_native(self._path, self.path_api_rel)
+
+    def glob(self, pattern: str):
+        return (
+            type(self)(p, self._path_api, self._api) for p in self._path.glob(pattern)
+        )
+
+    def rglob(self, pattern: str):
+        return (
+            type(self)(p, self._path_api, self._api) for p in self._path.rglob(pattern)
+        )
+
+
+class PathAPItracked(Path):
+    def __init__(
+        self,
+        path: str | Path,
+        file_id: str | None,
+        path_api: str | None,
+        api,
+        api_upload_type: str | None,
+    ):
+        """
+
+        Args:
+            path: local path
+            file_id: file ID on API for downloading
+            path_api: relative path on API for uploading
+            api: api binder
+            api_upload_type: type of upload, e.g. "artifact"
+        """
+        super().__init__(path)
+
+        self._path_api = path_api
+        self._api = api
+        self._api_upload_type = api_upload_type
+
+    def get(self):
+        pass
+
 
 class Uploader:
     @abstractmethod
--- test/unit/inout/test_files.py
+++ test/unit/inout/test_files.py
@@ -6,6 +6,30 @@ import pytest
 from fetcher.io import files
 
 
+@pytest.mark.parametrize("delegate", ["rglob", "glob"])
+def test_path_api_up_construction(delegate, tmpdir):
+    # tests that construction works from methods
+    [(tmpdir / p).write_text("abc", encoding="utf8") for p in ["a.txt", "b.txt", "c.txt"]]
+
+    p = files.PathAPIUp(Path(tmpdir), Path(""), None)
+    p_files = list(getattr(p, delegate)("*"))
+
+    for p in p_files:
+        assert isinstance(p, files.PathAPIUp)
+
+
+def test_path_api_up_push(tmpdir):
+    p = Path(tmpdir) / "test.txt"
+    p.write_text("test")
+
+    mock_api = mock.MagicMock()
+
+    p = files.PathAPIUp(p, tmpdir, mock_api)
+    p.push()
+
+    mock_api.put_file_native.assert_called_once_with(p._path, Path("test.txt"))
+
+
 @pytest.mark.parametrize("filename", [None, "test.txt"])
 def test_api_uploader(tmpdir, filename):
     url = "http://localhost:8000/file"

cb7bbd52ccf4be9daaeb791e0760efd5fe6eb157
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2023-09-21 15:51:36 +0200

    simplify

--- cli/main.py
+++ cli/main.py
@@ -3,23 +3,23 @@ import os
 import time
 from pathlib import Path
 
-import dotenv
 import docker
+import dotenv
+from loguru import logger
 
 from fetcher import api, info, io, status
 from fetcher.docker import manager
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(override=True)
 
 TIMEOUT = os.getenv("TIMEOUT", 10)
 TIMEOUT_MONITOR = os.getenv("TIMEOUT_MONITOR", 2)
 
 path_base = os.getenv("PATH_BASE", "~/temp/decode_cloud/mounts")
 path_base = Path(path_base).expanduser()
-path_host_base = os.getenv("PATH_HOST_BASE", "~/temp/decode_cloud/mounts")
-path_host_base = Path(path_host_base).expanduser()
+path_host_base = os.getenv("PATH_HOST_BASE")
 
-api_worker = api.worker.API(os.getenv("URL_BASE"), os.getenv("ACCESS_TOKEN"))
+api_worker = api.worker.API(os.getenv("API_URL"), os.getenv("ACCESS_TOKEN"))
 worker_info = info.sys.collect()
 
 
@@ -30,78 +30,79 @@ while True:
         environment="local",
         cpu_cores=worker_info["sys"]["cores"],
         memory=worker_info["sys"]["memory"],
-        gpu_model="3090",  # worker_info["gpu"][0]["model"] if worker_info["gpu"] else None,
-        gpu_memory=999999999,  # worker_info["gpu"][0]["memory"] if worker_info["gpu"] else None,
+        gpu_model=worker_info["gpu"][0]["model"] if worker_info["gpu"] else None,
+        gpu_memory=worker_info["gpu"][0]["memory"] if worker_info["gpu"] else None,
     )
 
-    if len(jobs) >= 1:
-        if len(jobs) >= 2:
-            raise ValueError(f"Expected only one job, got {len(jobs)}")
+    if len(jobs) == 0:
+        logger.info(f"No job found. Sleeping for {TIMEOUT} seconds.")
+        time.sleep(TIMEOUT)
+        continue
 
-        job_id, job = jobs.popitem()
-        api_job = api.worker.JobAPI(job_id, api_worker)
+    if len(jobs) >= 2:
+        raise ValueError(f"Expected only one job, got {len(jobs)}")
 
-        path_job = path_base / job_id
+    job_id, job = jobs.popitem()
+    api_job = api.worker.JobAPI(job_id, api_worker)
 
-        handler = job.handler
-        handler.files_up = [
-            io.files.PathAPIUp(path_job / p, path_job, api_job)
-            for p_api, p in handler.files_up.items()
-        ]
-        handler.files_down = [
-            io.files.PathAPIDown(path_job / p, p_id, api_job)
-            for p, p_id in handler.files_down.items()
-        ]
+    path_job = path_base / job_id
 
-        [p.get() for p in handler.files_down]
-        [p.mkdir(exist_ok=True, parents=True) for p in handler.files_up]
+    handler = job.handler
+    handler.files_up = [
+        io.files.PathAPIUp(path_job / p, path_job, api_job)
+        for p_api, p in handler.files_up.items()
+    ]
+    handler.files_down = [
+        io.files.PathAPIDown(path_job / p, p_id, api_job)
+        for p, p_id in handler.files_down.items()
+    ]
 
-        # here we need the paths on the host, we can not do this recursively
-        path_mnt = path_host_base / path_job.relative_to(path_base)
-        mounts = [
-            docker.types.Mount(
-                "/data",
-                str(path_mnt),
-                type="bind",
-                read_only=False,
-            ),
-        ]
-        docker_manager = manager.Manager(
-            image=job.handler.image_url,
-        )
-        kwargs_gpu = (
-            {
-                "device_requests": [
-                    docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])
-                ],
-            }
-            if worker_info["gpu"]
-            else {}
-        )
-        container = docker_manager.auto_run(
-            command=job.app.cmd,
-            environment=job.app.env,
-            mounts=mounts,
-            detach=True,
-            ipc_mode="host",
-            **kwargs_gpu,
-        )
+    [p.get() for p in handler.files_down]
+    [p.mkdir(exist_ok=True, parents=True) for p in handler.files_up]
 
-        # setup pinger
-        docker_stat = status.status.DockerStatus(container, ping=api_job.ping)
+    # here we need the paths on the host, we can not do this recursively
+    path_mnt = path_host_base / path_job.relative_to(path_base)
+    mounts = [
+        docker.types.Mount(
+            "/data",
+            str(path_mnt),
+            type="bind",
+            read_only=False,
+        ),
+    ]
+    docker_manager = manager.Manager(
+        image=job.handler.image_url,
+    )
+    kwargs_gpu = (
+        {
+            "device_requests": [
+                docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])
+            ],
+        }
+        if worker_info["gpu"]
+        else {}
+    )
+    container = docker_manager.auto_run(
+        command=job.app.cmd,
+        environment=job.app.env,
+        mounts=mounts,
+        detach=True,
+        ipc_mode="host",
+        **kwargs_gpu,
+    )
 
-        # get and keep updating its status
-        while True:
-            docker_stat.ping()
-            if docker_stat.exited:
-                print(container.logs())
-                break
+    # setup pinger
+    docker_stat = status.status.DockerStatus(container, ping=api_job.ping)
 
-            time.sleep(TIMEOUT_MONITOR)
+    # get and keep updating its status
+    while True:
+        docker_stat.ping()
+        if docker_stat.exited:
+            print(container.logs())
+            break
 
-        # upload result
-        p_upload = itertools.chain(*[p.rglob("*") for p in handler.files_up])
-        [p.push() for p in p_upload if p.is_file()]
+        time.sleep(TIMEOUT_MONITOR)
 
-    # Sleep for a defined interval before checking for the next job
-    time.sleep(TIMEOUT)
+    # upload result
+    p_upload = itertools.chain(*[p.rglob("*") for p in handler.files_up])
+    [p.push() for p in p_upload if p.is_file()]

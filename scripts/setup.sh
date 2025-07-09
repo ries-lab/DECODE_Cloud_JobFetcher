09315b34173cb96d2f4e0d28c6eda2d9a0c4b304
nolan1999 <arthur.jaques@hispeed.ch>
2025-07-09 19:35:27 +0200

    Required poetry version, correct poetry-core versioning

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,3 @@
-pip install "poetry==1.8.4"
+POETRY_VERSION=$(grep -Po '(?<=^requires-poetry\s*=\s*")[0-9]+\.[0-9]+\.[0-9]+(?=")' pyproject.toml)
+pip install "poetry==$POETRY_VERSION"
 poetry install --no-dev --no-interaction --no-ansi
